e2776e83f2b332d90e5c1d1f54d132cc9a21b26f
MiguelCigano <miguel_m0130@hotmail.com>
2022-10-24 08:28:44 -0700

    basic data

---
+++ tipos_datos.cpp
@@ -0,0 +1,17 @@
+#include<iostream>  //principal library
+
+using namespace std; // using cout and el cint
+
+//main function
+
+int main() {
+    int entero = 15;
+    float flotante_1 = 0.987;
+    double doble_ext = 8.09483992857;
+    char letra = 'a';
+
+    //cout<<flotante_1;
+    cout<<letra;
+    return 0;
+
+}
